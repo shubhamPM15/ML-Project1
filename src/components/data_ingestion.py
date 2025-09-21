eac130f245b003e85e12a82da42d25909302661a
ML Project <malpureshubham21@gmail.com>
2025-09-21 23:31:29 +0530

    data ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -22,20 +22,13 @@ class DataIngestion:
         try:
             df=pd.read_csv('notebook\stud.csv')
             logging.info('Read the dataset as dataframe')
-
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
-
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
-
             logging.info("Train test split initiated")
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
-
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
-
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
-
             logging.info("Inmgestion of the data iss completed")
-
             return(
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
